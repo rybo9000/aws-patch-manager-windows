8b8db79cb85f050ff842a6889fd07379001a4fe3
Ryan Fielder <15254619+rybo9000@users.noreply.github.com>
2025-04-01 16:57:48 -0500

    Corrected syntax to better fit tutorial article

--- providers.tf
+++ providers.tf
@@ -1,6 +1,5 @@
 provider "aws" {
   region = "us-east-1"
-  #   access_key = "ASDFA9ASDF09JASDF9JADF"
-  #   secret_key = "ASDF09QWEF0ASFASDF092F"
-  profile = "temp-sandbox"
+     access_key = "ASDFA9ASDF09JASDF9JADF"
+     secret_key = "ASDF09QWEF0ASFASDF092F"
 }
